529ba08a5f19ca5e58c9b1e44035ee735783166d
Jasper Ketelaar <Jasperketelaar@kpnmail.nl>
2019-11-18 22:57:43 +0100

    Added test cases for XOR function and recognizing braille alphabet

--- src/test/java/nl/yasper/neuralib/BrailleTest.java
+++ src/test/java/nl/yasper/neuralib/BrailleTest.java
@@ -1,4 +1,82 @@
 package nl.yasper.neuralib;
 
+import nl.yasper.neuralib.network.NeuralNetwork;
+import nl.yasper.neuralib.network.activation.ActivationFunction;
+import nl.yasper.neuralib.network.builder.LayerBuilder;
+import nl.yasper.neuralib.network.layer.InputLayer;
+import nl.yasper.neuralib.network.layer.PerceptronLayer;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class BrailleTest {
+
+    @Test
+    public void testBrailleAlphabet() {
+        PerceptronLayer inputLayer = new InputLayer(6);
+
+        PerceptronLayer hidden = new LayerBuilder(5)
+                .withActivationFunction(ActivationFunction.SIGMOID)
+                .withLearningRate(.2)
+                .withPerceptrons(6, 5)
+                .build();
+
+        PerceptronLayer output = new LayerBuilder(30)
+                .withActivationFunction(ActivationFunction.SIGMOID)
+                .withLearningRate(.2)
+                .withPerceptrons(5, 30)
+                .build();
+
+        NeuralNetwork neuralNetwork = new NeuralNetwork(inputLayer, new PerceptronLayer[]{hidden}, output);
+
+
+        double[][] inputs = {
+                {1, 0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0},
+                {1, 0, 1, 0, 1, 1},
+                {1, 1, 1, 1, 0, 0},
+                {1, 1, 0, 1, 0, 0},
+                {1, 1, 0, 1, 0, 1},
+                {1, 0, 0, 1, 0, 0},
+                {0, 1, 1, 0, 1, 1},
+                {1, 0, 0, 1, 1, 1},
+                {0, 1, 1, 0, 0, 0},
+                {0, 1, 1, 1, 0, 0},
+                {1, 0, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 0},
+                {1, 0, 1, 0, 0, 1},
+                {1, 1, 0, 0, 1, 0},
+                {1, 1, 0, 1, 1, 0},
+                {1, 1, 1, 0, 0, 1},
+                {1, 0, 0, 1, 1, 0},
+                {1, 1, 1, 0, 1, 0},
+                {1, 0, 1, 1, 1, 0},
+                {0, 1, 1, 0, 1, 0},
+                {0, 1, 1, 1, 1, 0},
+                {1, 1, 0, 0, 0, 1},
+                {1, 0, 0, 0, 1, 1},
+                {1, 1, 1, 0, 0, 0},
+                {1, 0, 1, 1, 0, 0},
+                {1, 1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 0, 1},
+                {1, 0, 0, 1, 0, 1},
+        };
+
+
+        double[][] outputs = new double[30][30];
+        for (int i = 0; i < outputs.length; i++) {
+            outputs[i] = out(i);
+        }
+
+        neuralNetwork.trainUntil(inputs, outputs, .1);
+        for (int i = 0; i < outputs.length; i++) {
+            Assert.assertEquals(neuralNetwork.binaryPredict(inputs[i]), outputs[i]);
+        }
+    }
+
+    private double[] out(int index) {
+        double[] result = new double[30];
+        result[index] = 1;
+        return result;
+    }
 }
--- src/test/java/nl/yasper/neuralib/XORTest.java
+++ src/test/java/nl/yasper/neuralib/XORTest.java
@@ -1,4 +1,46 @@
 package nl.yasper.neuralib;
 
+import nl.yasper.neuralib.network.NeuralNetwork;
+import nl.yasper.neuralib.network.activation.ActivationFunction;
+import nl.yasper.neuralib.network.builder.LayerBuilder;
+import nl.yasper.neuralib.network.layer.InputLayer;
+import nl.yasper.neuralib.network.layer.PerceptronLayer;
+import nl.yasper.neuralib.network.perceptron.LearningPerceptron;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
 public class XORTest {
+
+    @Test
+    public void testXORNetwork() {
+        System.out.println("Testing XOR network");
+        PerceptronLayer inputLayer = new InputLayer(2);
+
+        PerceptronLayer<LearningPerceptron> hidden = new LayerBuilder(2)
+                .withActivationFunction(ActivationFunction.SIGMOID)
+                .withLearningRate(.1)
+                .withPerceptrons(2, 2)
+                .build();
+
+        PerceptronLayer<LearningPerceptron> output = new LayerBuilder(1)
+                .withActivationFunction(ActivationFunction.SIGMOID)
+                .withLearningRate(.1)
+                .withPerceptrons(2, 1)
+                .build();
+
+        NeuralNetwork neuralNetwork = new NeuralNetwork(inputLayer, new PerceptronLayer[]{hidden}, output);
+
+        double[][] inputs = {{1, 1}, {0, 1}, {1, 0}, {0, 0}};
+        double[][] outputs = {{0}, {1}, {1}, {0}};
+
+        neuralNetwork.trainUntil(inputs, outputs, 0.001);
+
+
+        Assert.assertEquals(neuralNetwork.binaryPredict(new double[]{0, 1}), new double[]{1});
+        Assert.assertEquals(neuralNetwork.binaryPredict(new double[]{1, 0}), new double[]{1});
+        Assert.assertEquals(neuralNetwork.binaryPredict(new double[]{0, 0}), new double[]{0});
+        Assert.assertEquals(neuralNetwork.binaryPredict(new double[]{1, 1}), new double[]{0});
+    }
 }
